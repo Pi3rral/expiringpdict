1b32171cb661c2a5d7e19db63ba9a94dbca97528
Pierre Leca <pierre.leca@alayacare.com>
2016-05-25 14:56:01 -0400

    tag name

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'expiringpdict',
     packages = ['expiringpdict'],
-    version = '0.1',
+    version = 'r0.1',
     description = 'A Python Expiring Dictionary',
     author = 'Pi3rral',
     author_email = 'pierre.leca@gmail.com',
     url = 'https://github.com/Pi3rral/expiringpdict',
-    download_url = 'https://github.com/Pi3rral/expiringpdict/tarball/0.1',
+    download_url = 'https://github.com/Pi3rral/expiringpdict/tarball/r0.1',
     keywords = ['python', 'expiring', 'dict'],
     classifiers = [],
 )
