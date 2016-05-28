4a865f70ce79439674d6afd1a8952a89b7fd7842
Pierre Leca <pierre.leca@alayacare.com>
2016-05-27 22:05:15 -0400

    travis auto push to pypi r1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='expiringpdict',
     packages=['expiringpdict'],
-    version='0.2',
+    version='1.0',
     description='A Python Expiring Dictionary',
     author='Pi3rral',
     author_email='pierre.leca@gmail.com',
     url='https://github.com/Pi3rral/expiringpdict',
-    download_url='https://github.com/Pi3rral/expiringpdict/tarball/r0.2',
+    download_url='https://github.com/Pi3rral/expiringpdict/tarball/r1.0',
     keywords=['python', 'expiring', 'dict'],
     classifiers=[],
 )
