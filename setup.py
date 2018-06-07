3dbc1c09a9dc2718fe6367a9e115e7aee9306762
Pierre Leca <pierre.leca@alayacare.com>
2018-06-07 13:53:44 -0400

    add redis_db and redis_password parameters

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='expiringpdict',
     packages=['expiringpdict'],
-    version='1.1.2',
+    version='1.1.3',
     description='A Python Expiring Dictionary',
     author='Pi3rral',
     author_email='pierre.leca@gmail.com',
     url='https://github.com/Pi3rral/expiringpdict',
-    download_url='https://github.com/Pi3rral/expiringpdict/tarball/r1.1.2',
+    download_url='https://github.com/Pi3rral/expiringpdict/tarball/r1.1.3',
     keywords=['python', 'expiring', 'dict'],
     classifiers=[],
 )
